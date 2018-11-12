5ff8610d3d1b2f89245b60e9592dfcf18e4a50e3
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2018-11-12 14:54:28 +0300

    person function definition and lots of variables

--- ECH/main.py
+++ ECH/main.py
@@ -3,12 +3,15 @@ pygame.init()
 
 DISPLAY_WIDTH = 1024
 DISPLAY_HEIGHT = 720
+GROUND_HEIGHT = 10
 DISPLAY_TITLE = 'Demo'
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 PERSON_IMAGE = pygame.image.load('Person.png')
 PERSON_IMAGE_WIDTH = 71
 PERSON_IMAGE_HEIGHT = 127
+PERSON_POSITION_X = DISPLAY_WIDTH * 0.25 - PERSON_IMAGE_WIDTH
+PERSON_POSITION_Y = DISPLAY_HEIGHT - PERSON_IMAGE_HEIGHT - GROUND_HEIGHT
 
 
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
@@ -16,6 +19,11 @@ pygame.display.set_caption(DISPLAY_TITLE)
 clock = pygame.time.Clock()
 
 crashed = False
+
+
+def person(x, y):
+    gameDisplay.blit(PERSON_IMAGE, (x, y))
+
 
 while not crashed:
     for event in pygame.event.get():
@@ -27,6 +35,7 @@ while not crashed:
             print(event)
 
     gameDisplay.fill(COLOR_WHITE)
+    person(PERSON_IMAGE, (PERSON_POSITION_X, PERSON_POSITION_Y))
 
     pygame.display.update()
     clock.tick(10)
