f7de675095160a7cf9763989c71c16771676f257
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2018-11-12 14:41:51 +0300

    image added

--- ECH/main.py
+++ ECH/main.py
@@ -1,9 +1,18 @@
 import pygame
-
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((1024, 720))
-pygame.display.set_caption('A bit Racey')
+DISPLAY_WIDTH = 1024
+DISPLAY_HEIGHT = 720
+DISPLAY_TITLE = 'Demo'
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+PERSON_IMAGE = pygame.image.load('Person.png')
+PERSON_IMAGE_WIDTH = 71
+PERSON_IMAGE_HEIGHT = 127
+
+
+gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption(DISPLAY_TITLE)
 clock = pygame.time.Clock()
 
 crashed = False
@@ -14,7 +23,8 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+        if event.type == pygame.KEYDOWN:
+            print(event)
 
     pygame.display.update()
     clock.tick(10)
